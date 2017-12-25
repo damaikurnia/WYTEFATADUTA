f8d87e803e85e785740726c607719762a07aff6d
damaikurnia <damaikurnia@DAMAIKURNIA-100-14IBD>
2017-12-25 22:59:45 +0700

    [12-05-2017] Koneksi

--- KLINIKEFATA/src/Koneksi/Koneksi.java
+++ KLINIKEFATA/src/Koneksi/Koneksi.java
@@ -14,17 +14,18 @@ import java.sql.DriverManager;
  */
 public class Koneksi {
     //pasw google ph6CX4Em8ju3
-    private static String jdbcURL = "jdbc:mysql://localhost:3306/pgsd";
+    private static String jdbcURL = "jdbc:mysql://localhost:3306/DUTAEFATA";
     private static String user = "root";
 //    private static String password = "root";//linux database
-    private static String password = "";//windows database
+    private static String password = "mysqlroot";//windows database
     private static Connection connection = null;
 
     public static Connection getKoneksi() {
         try {
-            Class.forName("org.gjt.mm.mysql.Driver");
+//            Class.forName("org.gjt.mm.mysql.Driver");
+            Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, user, password);
-//            System.out.println("koneksi berhasil");
+            System.out.println("koneksi berhasil");
             return connection;
         } catch (Exception ex) {
             System.out.println("message: " + ex.getMessage() + "\nTidak konek");
